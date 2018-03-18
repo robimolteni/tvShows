97dfeaec03a0c3fe012bf8bfbf7cad6005d13093
robimolteni@gmail.com <robimolteni@gmail.com>
2018-03-18 21:09:37 +0100

    Removed useless fields

--- app/src/main/java/com/test/tvshow/model/TVShow.java
+++ app/src/main/java/com/test/tvshow/model/TVShow.java
@@ -13,30 +13,16 @@ import java.util.List;
 
 public class TVShow implements Parcelable {
 
-    private final String original_name;
-    private final List<Integer> genre_ids = null;
     private final String name;
-    private final double popularity;
-    private final List<String> origin_country;
-    private final int vote_count;
-    private final String first_air_date;
     private final String backdrop_path;
-    private final String original_language;
     private final int id;
     private final double vote_average;
     private final String overview;
     private final String poster_path;
 
-    public TVShow(String original_name, List<Integer> genre_ids, String name, double popularity, List<String> origin_country, int vote_count, String first_air_date, String backdrop_path, String original_language, int id, double vote_average, String overview, String poster_path){
-        this.original_name = original_name;
-       // this.genre_ids = genre_ids;
+    public TVShow(String name, String backdrop_path, int id, double vote_average, String overview, String poster_path){
         this.name = name;
-        this.popularity= popularity;
-        this.origin_country = origin_country;
-        this.vote_count = vote_count;
-        this.first_air_date = first_air_date;
         this.backdrop_path = backdrop_path;
-        this.original_language = original_language;
         this.id = id;
         this.vote_average = vote_average;
         this.overview = overview;
@@ -44,14 +30,8 @@ public class TVShow implements Parcelable {
     }
 
     protected TVShow(Parcel in) {
-        original_name = in.readString();
         name = in.readString();
-        popularity = in.readDouble();
-        origin_country = in.createStringArrayList();
-        vote_count = in.readInt();
-        first_air_date = in.readString();
         backdrop_path = in.readString();
-        original_language = in.readString();
         id = in.readInt();
         vote_average = in.readDouble();
         overview = in.readString();
@@ -60,14 +40,8 @@ public class TVShow implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(original_name);
         dest.writeString(name);
-        dest.writeDouble(popularity);
-        dest.writeStringList(origin_country);
-        dest.writeInt(vote_count);
-        dest.writeString(first_air_date);
         dest.writeString(backdrop_path);
-        dest.writeString(original_language);
         dest.writeInt(id);
         dest.writeDouble(vote_average);
         dest.writeString(overview);
@@ -91,40 +65,8 @@ public class TVShow implements Parcelable {
         }
     };
 
-    public String getOriginal_name() {
-        return original_name;
-    }
-
-    public List<Integer> getGenre_ids() {
-        return genre_ids;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public List<String> getOrigin_country() {
-        return origin_country;
-    }
-
-    public int getVote_count() {
-        return vote_count;
-    }
-
-    public String getFirst_air_date() {
-        return first_air_date;
-    }
-
-    public String getBackdrop_path() {
-        return backdrop_path;
-    }
-
-    public String getOriginal_language() {
-        return original_language;
     }
 
     public int getId() {
@@ -137,10 +79,6 @@ public class TVShow implements Parcelable {
 
     public String getOverview() {
         return overview;
-    }
-
-    public String getPoster_path() {
-        return poster_path;
     }
 
     public String getBackdropImageURL() {
